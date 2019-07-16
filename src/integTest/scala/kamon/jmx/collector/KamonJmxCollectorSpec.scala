f997047b91417bf4b15eafa20dba983b743c6a99
João Ferreira <jtjeferreira@gmail.com>
2019-07-16 19:13:10 +0100

    fix assertions

--- src/integTest/scala/kamon/jmx/collector/KamonJmxCollectorSpec.scala
+++ src/integTest/scala/kamon/jmx/collector/KamonJmxCollectorSpec.scala
@@ -9,7 +9,6 @@ import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, Produce
 import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringSerializer}
 import org.scalatest.{Assertion, FlatSpec}
 import org.scalatest.Matchers._
-import org.scalatest.Inspectors._
 import org.scalatest.concurrent.Eventually
 import org.scalatest.time.{Minute, Seconds, Span}
 import scala.collection.JavaConverters._
@@ -19,7 +18,7 @@ class KamonJmxCollectorSpec extends FlatSpec with Eventually {
   implicit override val patienceConfig: PatienceConfig =
     PatienceConfig(timeout = scaled(Span(1, Minute)), interval = scaled(Span(5, Seconds)))
 
-  def doSomeEntriesBeginWith(lines: List[String], beginWith: String): Assertion = forAll(lines) { line => line should startWith(beginWith) }
+  def doSomeEntriesBeginWith(lines: List[String], beginWith: String): Assertion = atLeast(1, lines) should startWith(beginWith) 
 
   "Kamon JMX collector" should "successfully collect JMX metrics and publish them to Kamon" in {
 
@@ -55,25 +54,25 @@ class KamonJmxCollectorSpec extends FlatSpec with Eventually {
     eventually {
       val metrics = HttpClient.getLinesFromURL("http://localhost:9095/")
 
-      doSomeEntriesBeginWith(metrics, "jmx_os_mbean_AvailableProcessors") shouldBe true
-      doSomeEntriesBeginWith(metrics, "jmx_os_memory_HeapMemoryUsage_committed") shouldBe true
-      doSomeEntriesBeginWith(metrics, "jmx_os_memory_HeapMemoryUsage_max") shouldBe true
-      doSomeEntriesBeginWith(metrics, "jmx_os_memory_ObjectPendingFinalizationCount") shouldBe true
+      doSomeEntriesBeginWith(metrics, "jmx_os_mbean_AvailableProcessors")
+      doSomeEntriesBeginWith(metrics, "jmx_os_memory_HeapMemoryUsage_committed")
+      doSomeEntriesBeginWith(metrics, "jmx_os_memory_HeapMemoryUsage_max")
+      doSomeEntriesBeginWith(metrics, "jmx_os_memory_ObjectPendingFinalizationCount")
 
-      doSomeEntriesBeginWith(metrics, """jmx_kafka_consumer_connection_count_bucket{type="consumer-metrics",client_id="consumer-1"""") shouldBe true
-      doSomeEntriesBeginWith(metrics, """jmx_kafka_consumer_connection_count_bucket{type="consumer-metrics",client_id="consumer-2"""") shouldBe true
+      doSomeEntriesBeginWith(metrics, """jmx_kafka_consumer_connection_count_bucket{type="consumer-metrics",client_id="consumer-1"""")
+      doSomeEntriesBeginWith(metrics, """jmx_kafka_consumer_connection_count_bucket{type="consumer-metrics",client_id="consumer-2"""")
 
-      doSomeEntriesBeginWith(metrics, """jmx_kafka_producer1_outgoing_byte_rate{client_id="producer-1",type="producer-metrics"}""") shouldBe true
-      doSomeEntriesBeginWith(metrics, """jmx_kafka_producer1_network_io_rate{client_id="producer-1",type="producer-metrics"}""") shouldBe true
+      doSomeEntriesBeginWith(metrics, """jmx_kafka_producer1_outgoing_byte_rate{client_id="producer-1",type="producer-metrics"}""")
+      doSomeEntriesBeginWith(metrics, """jmx_kafka_producer1_network_io_rate{client_id="producer-1",type="producer-metrics"}""")
 
-      doSomeEntriesBeginWith(metrics, """jmx_kafka_producer2_node_metrics_incoming_byte_rate{type="producer-node-metrics",node_id="node-1",client_id="producer-1"}""") shouldBe true
-      doSomeEntriesBeginWith(metrics, """jmx_kafka_producer2_node_metrics_outgoing_byte_rate{type="producer-node-metrics",node_id="node-1",client_id="producer-1"}""") shouldBe true
-      doSomeEntriesBeginWith(metrics, """jmx_kafka_producer2_node_metrics_incoming_byte_rate{type="producer-node-metrics",node_id="node--1",client_id="producer-1"}""") shouldBe true
-      doSomeEntriesBeginWith(metrics, """jmx_kafka_producer2_node_metrics_outgoing_byte_rate{type="producer-node-metrics",node_id="node--1",client_id="producer-1"}""") shouldBe true
+      doSomeEntriesBeginWith(metrics, """jmx_kafka_producer2_node_metrics_incoming_byte_rate{type="producer-node-metrics",node_id="node-1",client_id="producer-1"}""")
+      doSomeEntriesBeginWith(metrics, """jmx_kafka_producer2_node_metrics_outgoing_byte_rate{type="producer-node-metrics",node_id="node-1",client_id="producer-1"}""")
+      doSomeEntriesBeginWith(metrics, """jmx_kafka_producer2_node_metrics_incoming_byte_rate{type="producer-node-metrics",node_id="node--1",client_id="producer-1"}""")
+      doSomeEntriesBeginWith(metrics, """jmx_kafka_producer2_node_metrics_outgoing_byte_rate{type="producer-node-metrics",node_id="node--1",client_id="producer-1"}""")
 
-      doSomeEntriesBeginWith(metrics, """jmx_kafka_consumer_fetch_manager_topic_bytes_consumed_rate{topic="test",type="consumer-fetch-manager-metrics",client_id="consumer-1"}""") shouldBe true
-      doSomeEntriesBeginWith(metrics, """jmx_kafka_consumer_fetch_manager_records_lag_max{type="consumer-fetch-manager-metrics",client_id="consumer-1"}""") shouldBe true
-      doSomeEntriesBeginWith(metrics, """jmx_kafka_consumer_fetch_manager_records_lag_max{type="consumer-fetch-manager-metrics",client_id="consumer-2"}""") shouldBe true
+      doSomeEntriesBeginWith(metrics, """jmx_kafka_consumer_fetch_manager_topic_bytes_consumed_rate{topic="test",type="consumer-fetch-manager-metrics",client_id="consumer-1"}""")
+      doSomeEntriesBeginWith(metrics, """jmx_kafka_consumer_fetch_manager_records_lag_max{type="consumer-fetch-manager-metrics",client_id="consumer-1"}""")
+      doSomeEntriesBeginWith(metrics, """jmx_kafka_consumer_fetch_manager_records_lag_max{type="consumer-fetch-manager-metrics",client_id="consumer-2"}""")
     }
   }
 }
